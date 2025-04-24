e70cd7f17d54da3b5a74ed1dd62d6a8c2668e979
pol <polescola3000@gmail.com>
2025-04-24 12:01:05 +0200

    iq cahnges

--- rtl/datapath/rtl/ir_stage/rtl/instruction_queue.sv
+++ rtl/datapath/rtl/ir_stage/rtl/instruction_queue.sv
@@ -24,13 +24,11 @@ module instruction_queue
     input logic            clk_i,            // Clock Singal
     input logic            rstn_i,           // Negated Reset Signal
 
-    input id_ir_stage_t    instruction_1_i,    // All instruction input signals   
-    input id_ir_stage_t    instruction_2_i,    // All instruction input signals   
+    input id_ir_stage_t    instruction_S_i [NUM_SCALAR_INSTR],    // All instruction input signals   
     input logic            flush_i,          // Flush all entries to 0
-    input logic            read_head_i,      // Read tail of the ciruclar buffer
+    input logic            read_head_i[NUM_SCALAR_INSTR],      // Read tail of the ciruclar buffer
 
-    output id_ir_stage_t   instruction_1_o,    // All intruction output signals
-    output id_ir_stage_t   instruction_2_o,    // All intruction output signals
+    output id_ir_stage_t   instruction_S_o [NUM_SCALAR_INSTR],    // All intruction output signals
     output logic           full_o,           // IQ is full
     output logic           empty_o           // IQ is empty TODO: check if empty signal is necessary
 );
@@ -44,30 +42,30 @@ reg_instruction_queue_entry tail;
 //Num must be 1 bit bigger than head an tail
 logic [$clog2(INSTRUCTION_QUEUE_NUM_ENTRIES):0] num;
 
-logic write_enable_1;
-logic read_enable_1;
+logic write_enable_S[NUM_SCALAR_INSTR];
+logic read_enable_S[NUM_SCALAR_INSTR];
 
-logic write_enable_2;
-logic read_enable_2;
+
 
 // User can write to the head of the buffer if the new data is valid and
 // there are any free entry
-assign write_enable_1 = instruction_1_i.instr.valid & (num < INSTRUCTION_QUEUE_NUM_ENTRIES);
-assign write_enable_2 = instruction_2_i.instr.valid & (num < INSTRUCTION_QUEUE_NUM_ENTRIES-1);
-
-// User can read the tail of the buffer if there is data stored in the queue
-// or in this cycle a new entry is written
-assign read_enable_1 = read_head_i & (num > 0) ;
-assign read_enable_2 = read_head_i & (num > 1) ;
-
+always_comb begin
+    for(int i = 0; i<NUM_SCALAR_INSTR; i++) begin
+        write_enable_S[i] = instruction_S_i.instr[i].valid & (num < INSTRUCTION_QUEUE_NUM_ENTRIES-i);
+    // User can read the tail of the buffer if there is data stored in the queue
+    // or in this cycle a new entry is written
+        read_enable_S[i] = read_head_i & (num > 0+i);
+    end
+end
 
 id_ir_stage_t instruction_buffer[0:INSTRUCTION_QUEUE_NUM_ENTRIES-1];
 
 always_ff @(posedge clk_i)
 begin
-    if (write_enable_1) begin
-        instruction_buffer[tail] <= instruction_1_i;
-        instruction_buffer[tail+1] <= instruction_2_i;
+    for(int i =0; i < NUM_SCALAR_INSTR; i++) begin
+        if (write_enable_S[i]) begin
+            instruction_buffer[tail+i] <= instruction_S_i[i];
+        end
     end
 end
 
@@ -84,8 +82,8 @@ begin
         num  <= 4'b0;
     end
     else begin
-        head <= head + {2'b00, read_enable_1} + {2'b00, read_enable_2};
-        tail <= tail + {2'b00, write_enable_1} + {2'b00, write_enable_2};
+        head <= head + {2'b00, read_enable_S[0]} + {2'b00, read_enable_S[1]};
+        tail <= tail + {2'b00, write_enable_S[0]} + {2'b00, write_enable_S[1]};
         num  <= num  + {3'b0, write_enable_1} - {3'b0, read_enable_1} + {3'b0, write_enable_2} - {3'b0, read_enable_2}; 
     end
 end
@@ -94,8 +92,9 @@ end
 //assign instruction_1_o = (~read_enable_1)? 'h0 : instruction_buffer[head];
 
 //TODO: Beware!! This head-1 will make errors if no instruction 2 is on the instrucition queue
-assign instruction_1_o = empty_o? 'h0 :  instruction_buffer[head-read_enable_2];
-assign instruction_2_o = empty_o && read_enable_2? 'h0 : instruction_buffer[head];
+
+assign instruction_S_o[0] = empty_o ? 'h0 :  instruction_buffer[head-read_enable_S[0]];
+assign instruction_S_o[1] = empty_o && read_enable_S[1]? 'h0 : instruction_buffer[head];
 assign empty_o = (num == 0);
 assign full_o  = ((num == INSTRUCTION_QUEUE_NUM_ENTRIES) | ~rstn_i);
 
